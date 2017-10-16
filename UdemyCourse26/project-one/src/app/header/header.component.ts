a7474eb19ec9f60a8052876dc71aafe03a31af1d
Matheos Asfaw <matheos.asfaw@gmail.com>
2017-10-16 12:17:19 -0700

    Section 6 completed

--- UdemyCourse26/project-one/src/app/header/header.component.ts
+++ UdemyCourse26/project-one/src/app/header/header.component.ts
@@ -1,9 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output} from '@angular/core';
+
 
 @Component ({
   selector : 'app-header',
   templateUrl: './header.component.html'
 })
 export class HeaderComponent {
+
+  @Output() featureSelected  = new EventEmitter<string>();
+
+  onSelect(feature : string ){
+
+    console.log("On Select from header was triggered");
+
+    this.featureSelected.emit(feature);
+  }
 
 }
--- UdemyCourse26/project-one/src/app/recipes/recipe-list/recipe-list.component.ts
+++ UdemyCourse26/project-one/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit ,Output, EventEmitter } from '@angular/core';
 import {Recipe} from "../recipe.model";
 
 @Component({
@@ -8,14 +8,20 @@ import {Recipe} from "../recipe.model";
 })
 export class RecipeListComponent implements OnInit {
 
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
+
   recipes: Recipe []  = [
     new Recipe("A Test Recipe","This is simply a test" , 'http://technicallyteamann.com/wp-content/uploads/2017/03/Recipe_logo-1.jpeg' ),
-    new Recipe("A Test Recipe","This is simply a test" , 'http://technicallyteamann.com/wp-content/uploads/2017/03/Recipe_logo-1.jpeg' )
+    new Recipe("Another  Test Recipe","This is simply another  test" , 'http://technicallyteamann.com/wp-content/uploads/2017/03/Recipe_logo-1.jpeg' )
   ];
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onRecipeSelected( recipe : Recipe){
+    this.recipeWasSelected.emit(recipe);
   }
 
 }
