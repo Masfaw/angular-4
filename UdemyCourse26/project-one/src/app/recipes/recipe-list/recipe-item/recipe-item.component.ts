4285a4403c3e99dc65de7d402aed84a6ed6a9812
Matheos Asfaw <matheos.asfaw@gmail.com>
2017-11-13 09:40:09 -0800

    Finished section 12

--- UdemyCourse26/project-one/src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ UdemyCourse26/project-one/src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit, Input, EventEmitter, Output} from '@angular/core';
+import { Component, OnInit, Input} from '@angular/core';
 import { Recipe } from '../../recipe.model';
-import {RecipeService} from "../../recipe.service";
+
+
 @Component({
   selector: 'app-recipe-item',
   templateUrl: './recipe-item.component.html',
@@ -9,21 +10,10 @@ import {RecipeService} from "../../recipe.service";
 export class RecipeItemComponent implements OnInit {
 
   @Input() recipe: Recipe;
-
-  //
-  // @Output() recipeSelected = new EventEmitter<void>();
-
-
-  constructor(private recipeService : RecipeService) { }
+  @Input() index : number;
 
   ngOnInit() {
   }
 
-  onSelected () {
-    // this.recipeSelected.emit();
-
-    this.recipeService.recipeSelected.emit(this.recipe);
-
-  }
 
 }
